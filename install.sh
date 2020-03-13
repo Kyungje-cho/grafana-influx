3111dda3032f05970a8187bf750f968eecf7eda5
Simon Ogden <siytek@simons-mbp.home>
2020-03-13 23:04:01 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "====================================="
+echo "==---------------------------------=="
+echo "== Grafana & InfluxDB Installation =="
+echo "==                                 =="
+echo "==        From SIYTEK.COM          =="
+echo "==                                 =="
+echo "==   Electronics Do It Yourself!   =="
+echo "==---------------------------------=="
+echo "====================================="
+
